3684a34b5ae593a543c4a093792785eb299cf2ac
he44 <he44@illinois.edu>
2018-06-12 19:32:19 +0800

    bouncing ball in p5.js

--- tutorial/sketch.js
+++ tutorial/sketch.js
@@ -1,28 +1,56 @@
-var circle = {
-	x: 300,
-	y: 300,
-	diameter: 50
+var ball = {
+	x: 0,
+	y: 0,
+	xspeed: 2,
+	yspeed: 2,
+	size: 24
+}
+
+// width and height are reserved
+var var_width = 600, var_height = 400;
+var claycolor = {
+	r: 166, 
+	g: 61, 
+	b: 47
 };
 
-var grayscale = 0;
-
 function setup() {
-	createCanvas(600, 600);
+	createCanvas(var_width, var_height);
+	placeBall();
 }
 
 function draw() {
-	// background(26, 88, 188);
-	grayscale = map(mouseY, 0, 600, 0, 255);
-	/*
-		grayscale <--> mouseX: proportional
-	*/
-	background(grayscale);
-	fill(26, 88, 188);
-	noStroke();
-	ellipse(mouseX, mouseY, circle.diameter, circle.diameter);
-	// background(grayscale);
-	console.log(mouseX);
-	console.log(typeof(mouseX));
+	background(claycolor.r, claycolor.g, claycolor.b);
+	move();
+	bounce();
+	display();
 }
 
-// text editor
+function placeBall()
+{
+	ball.x = Math.random() * var_width;
+	ball.y = Math.random() * var_height;
+}
+
+function move()
+{
+	ball.x += ball.xspeed;
+	ball.y += ball.yspeed;
+}
+
+function bounce()
+{
+	if (ball.x + ball.size/2 > var_width || ball.x - ball.size/2 < 0){
+		ball.xspeed = -ball.xspeed;
+	}
+	if (ball.y + ball.size/2 > var_height || ball.y - ball.size/2 < 0){
+		ball.yspeed = -ball.yspeed;
+	}
+}
+
+function display()
+{
+	noStroke();
+	fill(40, 200, 0);
+	ellipse(ball.x, ball.y, ball.size, ball.size);
+}
