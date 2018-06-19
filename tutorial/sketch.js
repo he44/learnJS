029ed4a4bca823555e61a52ca8e067e4bd5ffe62
he44 <he44@illinois.edu>
2018-06-19 21:07:55 +0800

    constructor with argument

--- tutorial/sketch.js
+++ tutorial/sketch.js
@@ -1,21 +1,20 @@
-var ball = {
-	x: 0,
-	y: 0,
-	xspeed: 2,
-	yspeed: 2,
-	size: 24
-}
+// width and height are reserved
+let var_width = 600, var_height = 400;
+let claycolor = {
+	r: 166, 
+	g: 61, 
+	b: 47
+};
 
-let class_ball;
-
+let big_ball, small_ball;
 
 class Ball {
-	constructor(){
+	constructor(radius){
 		this.x = Math.random() * var_width;
 		this.y = Math.random() * var_height;
 		this.xspeed = 2;
 		this.yspeed = 2;
-		this.size = 24;
+		this.size = radius;
 	}
 	move(){
 		this.x += this.xspeed;
@@ -32,59 +31,22 @@ class Ball {
 	display(){
 		noStroke();
 		fill(40, 200, 0);
-		ellipse(this.x, this.y, this.size, this.size);
+		ellipse(this.x, this.y, this.size);
 	}
 }
 
-// width and height are reserved
-var var_width = 600, var_height = 400;
-var claycolor = {
-	r: 166, 
-	g: 61, 
-	b: 47
-};
-
 function setup() {
 	createCanvas(var_width, var_height);
-	class_ball = new Ball();
-	placeBall();
+	big_ball = new Ball(40);
+	small_ball = new Ball(24);
 }
 
 function draw() {
 	background(claycolor.r, claycolor.g, claycolor.b);
-	class_ball.move();
-	class_ball.bounce();
-	class_ball.display();
-	move();
-	bounce();
-	display();
-}
-
-function placeBall()
-{
-	ball.x = Math.random() * var_width;
-	ball.y = Math.random() * var_height;
-}
-
-function move()
-{
-	ball.x += ball.xspeed;
-	ball.y += ball.yspeed;
-}
-
-function bounce()
-{
-	if (ball.x + ball.size/2 > var_width || ball.x - ball.size/2 < 0){
-		ball.xspeed = -ball.xspeed;
-	}
-	if (ball.y + ball.size/2 > var_height || ball.y - ball.size/2 < 0){
-		ball.yspeed = -ball.yspeed;
-	}
-}
-
-function display()
-{
-	noStroke();
-	fill(255, 200, 0);
-	ellipse(ball.x, ball.y, ball.size, ball.size);
+	big_ball.move();
+	small_ball.move();
+	big_ball.bounce();
+	small_ball.bounce();
+	big_ball.display();
+	small_ball.display();
 }
