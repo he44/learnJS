eac6e090595ea41f8193713e6a111b1918d9646d
he44 <he44@illinois.edu>
2018-06-19 20:51:00 +0800

    two bouncing ball

--- tutorial/sketch.js
+++ tutorial/sketch.js
@@ -6,6 +6,36 @@ var ball = {
 	size: 24
 }
 
+let class_ball;
+
+
+class Ball {
+	constructor(){
+		this.x = Math.random() * var_width;
+		this.y = Math.random() * var_height;
+		this.xspeed = 2;
+		this.yspeed = 2;
+		this.size = 24;
+	}
+	move(){
+		this.x += this.xspeed;
+		this.y += this.yspeed;
+	}
+	bounce(){
+		if (this.x + this.size/2 > var_width || this.x - this.size/2 < 0){
+			this.xspeed = -this.xspeed;
+		}
+		if (this.y + this.size/2 > var_height || this.y - this.size/2 < 0){
+			this.yspeed = -this.yspeed;
+		}
+	}
+	display(){
+		noStroke();
+		fill(40, 200, 0);
+		ellipse(this.x, this.y, this.size, this.size);
+	}
+}
+
 // width and height are reserved
 var var_width = 600, var_height = 400;
 var claycolor = {
@@ -16,11 +46,15 @@ var claycolor = {
 
 function setup() {
 	createCanvas(var_width, var_height);
+	class_ball = new Ball();
 	placeBall();
 }
 
 function draw() {
 	background(claycolor.r, claycolor.g, claycolor.b);
+	class_ball.move();
+	class_ball.bounce();
+	class_ball.display();
 	move();
 	bounce();
 	display();
@@ -51,6 +85,6 @@ function bounce()
 function display()
 {
 	noStroke();
-	fill(40, 200, 0);
+	fill(255, 200, 0);
 	ellipse(ball.x, ball.y, ball.size, ball.size);
 }
