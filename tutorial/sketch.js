fd950786507d4590ce9260720b2bdd21ca72d0cd
he44 <he44@illinois.edu>
2018-06-21 16:31:42 +0800

    array and mouse

--- tutorial/sketch.js
+++ tutorial/sketch.js
@@ -1,52 +1,58 @@
 // width and height are reserved
 let var_width = 600, var_height = 400;
-let claycolor = {
-	r: 166, 
-	g: 61, 
-	b: 47
-};
 
-let big_ball, small_ball;
+let stars = [];
 
-class Ball {
-	constructor(radius){
-		this.x = Math.random() * var_width;
-		this.y = Math.random() * var_height;
-		this.xspeed = 2;
-		this.yspeed = 2;
-		this.size = radius;
-	}
-	move(){
-		this.x += this.xspeed;
-		this.y += this.yspeed;
-	}
-	bounce(){
-		if (this.x + this.size/2 > var_width || this.x - this.size/2 < 0){
-			this.xspeed = -this.xspeed;
-		}
-		if (this.y + this.size/2 > var_height || this.y - this.size/2 < 0){
-			this.yspeed = -this.yspeed;
-		}
-	}
-	display(){
-		noStroke();
-		fill(40, 200, 0);
-		ellipse(this.x, this.y, this.size);
-	}
-}
 
 function setup() {
 	createCanvas(var_width, var_height);
-	big_ball = new Ball(40);
-	small_ball = new Ball(24);
+	let num = random(20, 40);
+	for (let i = 0; i < num; i++){
+		stars[i] = new star(random(20,40));
+	}
 }
 
 function draw() {
-	background(claycolor.r, claycolor.g, claycolor.b);
-	big_ball.move();
-	small_ball.move();
-	big_ball.bounce();
-	small_ball.bounce();
-	big_ball.display();
-	small_ball.display();
+	background(0);
+	for (let star of stars) {
+		star.blink();
+		if(star.inbound(mouseX, mouseY)){star.changeColor();}
+		star.display();
+	}
+}
+
+// adding a new star to the array every time the mouse is clicked
+function mousePressed(){
+	for (let i = stars.length-1; i >= 0; i--){
+		if(stars[i].inbound(mouseX, mouseY)){
+			stars.splice(i,1);
+		}
+	}
+}
+
+
+
+class star {
+	constructor(radius){
+		this.x = Math.random() * var_width;
+		this.y = Math.random() * var_height;
+		this.size = radius;
+		this.color = color(255, 100, 100);
+	}
+	blink(){
+		this.x += random(-5,5);
+		this.y += random(-5,5);
+	}
+	changeColor(){
+		this.color = color(random(0,255), random(0,255), random(0,255));
+	}
+	inbound(x, y){
+		return(dist(x, y, this.x, this.y) < this.size);
+	}
+	display(){
+		stroke(255);
+		strokeWeight(2);
+		fill(this.color)
+		ellipse(this.x, this.y, this.size);
+	}
 }
