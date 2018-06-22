e993b3bfac9e4f9b4432955487e0f128dd64a405
he44 <he44@illinois.edu>
2018-06-22 20:30:58 +0800

    working clock

--- clock_assignment/clock.js
+++ clock_assignment/clock.js
@@ -1,5 +1,61 @@
-class Clock(){
+class Clock {
     constructor(){
-        
+        this.hour = 0;
+        this.minute = 0;
+        this.second = 0;
+        this.convention = "AM";
+        this.x = 300;
+        this.y = 200;
+        this.d = 200;
+    }
+    getCurrentTime(){
+        this.hour = hour();
+        this.minute = minute();
+        this.second = second();
+    }
+    displayTimeAsText(){
+        fill(255);
+        text(this.hour + ":" + this.minute + ":" + this.second, 200, 200);
+    }
+    displayTimeAsCircle(){
+        //  Getting the clock frame drawn
+        strokeWeight(6);
+        stroke(silver);
+        noFill();
+        arc(this.x, this.y, this.d+10, this.d+10, 0, 2*PI);
+        //  Getting the hour drawn
+        if(this.hour >= 12){
+            this.convention = "PM";
+            this.hour -= 12;
+        }
+        if(this.hour != 0){
+            let hour_angle = map(this.hour, 0, 12, -PI/2, 3*PI/2);
+            strokeWeight(8);
+            stroke(green);
+            noFill();
+            arc(this.x, this.y, this.d, this.d, -PI/2, hour_angle);
+        }
+        //  Getting the minute drawn
+        if(this.minute != 0){
+            let minute_angle = map(this.minute, 0, 60, -PI/2, 3*PI/2);
+            strokeWeight(8);
+            stroke(purple);
+            noFill();
+            arc(this.x, this.y, this.d-20, this.d-20, -PI/2, minute_angle);
+        }
+        //  Getting the second drawn
+        if(this.second != 0){
+            let second_angle = map(this.second, 0, 60, -PI/2, 3*PI/2);
+            strokeWeight(8);
+            stroke(pink);
+            noFill();
+            arc(this.x, this.y, this.d-40, this.d-40, -PI/2, second_angle);
+            console.log(second_angle);
+        }
+        // display AM/PM
+        noStroke();
+        fill(cobalt_blue);
+        textSize(32);
+        text(this.convention, this.x-20, this.y-20, 40, 40);
     }
 }
--- clock_assignment/sketch.js
+++ clock_assignment/sketch.js
@@ -1,7 +1,24 @@
+let clock;
+let cobalt_blue, light_yellow, vibrant_orange;
+let pink, green, purple, silver;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	// createCanvas(windowWidth, windowHeight);
+	createCanvas(600,400);
+	cobalt_blue = color(0, 71, 171);
+	light_yellow = color(255,255,0);
+	vibrant_orange = color(255,165,0);
+	pink = color(255,100,150);
+	green = color(150,255,100);
+	purple = color(150,100,255);
+	silver = color(192,192,192);
+	clock = new Clock();
 }
 
 function draw() {
-
+	background(50);
+	clock.getCurrentTime();
+	// clock.hour = 18;
+	// clock.displayTimeAsText();
+	clock.displayTimeAsCircle();
 }
